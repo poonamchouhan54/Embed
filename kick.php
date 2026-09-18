27481a130df0dcbc0ffc6d0dc3d24c19c6458156
poonamchouhan54 <71801966+poonamchouhan54@users.noreply.github.com>
2026-09-18 11:58:56 +0530

    Add kick.php for PRT Kick Stream embed

---
+++ kick.php
@@ -0,0 +1,42 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<meta charset="utf-8">
+<meta name="viewport" content="width=device-width,initial-scale=1">
+<title>PRT Kick Stream</title>
+<style>
+html, body {
+    margin: 0;
+    padding: 0;
+    background: #000;
+    width: 100%;
+    height: 100%;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    overflow: hidden;
+}
+.embed-container {
+    position: relative;
+    width: 100%;
+    height: 100vh;
+}
+.embed-container iframe {
+    position: absolute;
+    top: 0;
+    left: 0;
+    width: 100%;
+    height: 100%;
+    border: 0;
+}
+</style>
+</head>
+<body>
+
+<div class="embed-container">
+    <iframe src="https://kick.com/prt0895" frameborder="0" scrolling="no" allowfullscreen="true"></iframe>
+</div>
+
+</body>
+</html>
+
